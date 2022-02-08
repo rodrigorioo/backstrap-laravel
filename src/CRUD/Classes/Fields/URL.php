b52fecc45f8892c0b47cb7c139718cdbdfff9612
Rodrigo Rio <rodrigo.rio@outlook.com.ar>
2022-02-08 15:35:55 -0300

    URL Field

---
+++ src/CRUD/Classes/Fields/URL.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Rodrigorioo\BackStrapLaravel\CRUD\Classes\Fields;
+
+use Collective\Html\FormFacade;
+use Rodrigorioo\BackStrapLaravel\CRUD\Classes\Field;
+
+class URL extends Field {
+
+    public function render ($errors, $model = null) {
+
+        $value = null;
+        if($model !== null) {
+            $value = $model->{$this->getFieldName()};
+        }
+
+        $returnInput = FormFacade::label($this->getFieldName(), $this->getName());
+        $returnInput .= FormFacade::url($this->getFieldName(), $value, $this->getExtraData($errors));
+
+        // Error message
+        $returnInput .= $this->getErrorMessage($errors);
+
+        return $returnInput;
+    }
+}
