88913cb54dc0601196834fd2f9b9612fd463bb5b
Rodrigo Rio <rodrigo.rio@outlook.com.ar>
2022-03-09 12:55:41 -0300

    URL full

--- src/Traits/URL.php
+++ src/Traits/URL.php
@@ -2,6 +2,8 @@
 
 namespace Rodrigorioo\BackStrapLaravel\Traits;
 
+use Illuminate\Support\Arr;
+
 trait URL {
 
     public static function getLoginURL() : string {
@@ -10,6 +12,17 @@ trait URL {
 
     public static function getLogoutURL() : string {
         return config('backstrap_laravel.prefix').'/'.config('backstrap_laravel.logout_url');
+    }
+
+    public static function generateLanguageURL($url, $newQuery) : string {
+
+        // Get actual query
+        $query = request()->query();
+
+        // Replace actual query with new query
+        $fullQuery = array_merge($query, $newQuery);
+
+        return $url.'?'.Arr::query($fullQuery);
     }
 
 }
